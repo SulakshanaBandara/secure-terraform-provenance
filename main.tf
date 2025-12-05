32ef4a1804863001fbc5fbd8ba24c502fbe2c927
sulakshana karunarathne <sulakshanabandarawork@gmail.com>
2025-12-05 23:27:21 +0530

    Initial Terraform demo on macOS

---
+++ main.tf
@@ -0,0 +1,11 @@
+terraform {
+	required_version = ">= 1.0.0"
+}
+
+provider "local"{}
+
+resource "local_file" "example" {
+	filename = "${path.module}/hello.txt"
+	content = "Hellow from terraform on MacOs"
+}
+
